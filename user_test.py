87e273b0b5aba1828b1e439d40378f7ca496f4ba
vique254 <victoronyango051@gmail.com>
2019-11-11 16:25:18 +0300

    create a test to check if I  receive the list of saved accounts

--- user_test.py
+++ user_test.py
@@ -76,6 +76,13 @@ class TestAccount(unittest.TestCase):
         account_exists = Account.account_exist("0712345678")
 
         self.assertTrue(account_exists)
+        
+    def test_display_all_account(self):
+        '''
+        method that returns a list of all accounts saved
+        '''
+
+        self.assertEqual(Account.display_account(),Account.account_list)
 
 if __name__ == '__main__':
     unittest.main()
