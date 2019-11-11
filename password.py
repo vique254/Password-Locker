ceb1e4972d6ea5db53ed243186908d5df8828e4c
vique254 <victoronyango051@gmail.com>
2019-11-11 08:17:32 +0300

    create an instance and class variables

--- password.py
+++ password.py
@@ -15,3 +15,11 @@ class Account:
                 last_name : New account last name.
                 password : New account password address.
             '''
+  
+    def __init__(self,first_name,last_name,phone_number,email):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
