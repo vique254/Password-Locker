52325d4ed188a9178a37b629eeea7c7be21de96f
vique254 <victoronyango051@gmail.com>
2019-11-11 14:29:13 +0300

    create a find_by_number class method

--- password.py
+++ password.py
@@ -9,13 +9,14 @@ class Account:
 
         Account.account_list.append(self)
         
-    def __init__(self,first_name,last_name,password):
+    def __init__(self,first_name,last_name,number,email):
 
       # docstring removed for simplicity
 
         self.first_name = first_name
         self.last_name = last_name
-        self.password = password
+        self.phone_number = number
+        self.email = email
         
     def tearDown(self):
         '''
@@ -30,3 +31,18 @@ class Account:
         '''
 
         Account.account_list.remove(self)
+        
+    @classmethod
+    def find_by_number(cls,number):
+        '''
+        Method that takes in a number and returns a account that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Account of person that matches the number.
+        '''
+
+        for account in cls.account_list:
+            if account.phone_number == number:
+                return account
