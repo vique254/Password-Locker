d641ab18c20392982c3f0b641128a90fc9693873
vique254 <victoronyango051@gmail.com>
2019-11-11 14:32:44 +0300

    create a test for find_by_number class method

--- user_test.py
+++ user_test.py
@@ -12,7 +12,7 @@ class TestAccount(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_account = Account("vique","254","12345v") # create contact object
+        self.new_account = Account("victor","onyango","0700736350","victoronyango051@gmail.com")
 
 
     def test_init(self):
@@ -20,14 +20,15 @@ class TestAccount(unittest.TestCase):
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_account.first_name,"vique")
-        self.assertEqual(self.new_account.last_name,"254")
-        self.assertEqual(self.new_account.password,"12345v")
+        self.assertEqual(self.new_account.first_name,"victor")
+        self.assertEqual(self.new_account.last_name,"onyango")
+        self.assertEqual(self.new_account.phone_number,"0700736350")
+        self.assertEqual(self.new_account.email,"victoronyango051@gmail.com")
 
     def test_save_account(self):
        
         self.new_account.save_account() 
-        self.assertEqual(len(Account.account_list),2)
+        self.assertEqual(len(Account.account_list),4)
 
     def test_save_multiple_account(self):
         '''
@@ -35,20 +36,34 @@ class TestAccount(unittest.TestCase):
         objects to our account_list
         '''
         self.new_account.save_account()
-        test_account = Account("Richie","spice","00000") # new account
+        test_account = Account("Richie","spice","0712345678","Richiespice@yahoo.com") 
         test_account.save_account()
-        self.assertEqual(len(Account.account_list),4)
+        self.assertEqual(len(Account.account_list),6)
 
     def test_delete_contact(self):
         '''
         test_delete_account to test if we can remove a account from our account list
         '''
         self.new_account.save_account()
-        test_account = Account("Richie","spice","00000") 
+        test_account = Account("Richie","spice","0712345678","Richiespice@yahoo.com") 
         test_account.save_account()
 
         self.new_account.delete_account()
         self.assertEqual(len(Account.account_list),1)
+
+    def test_find_account_by_number(self):
+        '''
+        test to check if we can find a account by phone number and display information
+        '''
+
+        self.new_account.save_account()
+        test_account = Account("Richie","spice","0712345678","Richiespice@yahoo.com") 
+        test_account.save_account()
+
+        found_account = Account.find_by_number("0712345678")
+
+        self.assertEqual(found_account.email,test_account.email)
+
 
 if __name__ == '__main__':
     unittest.main()
