5ac894c81fe5cd58037358198171e8bf4fb144f1
vique254 <victoronyango051@gmail.com>
2019-11-11 08:10:38 +0300

    create a class Account

---
+++ password.py
@@ -0,0 +1,17 @@
+class Account:
+    """
+    Class that generates new instances of passwords
+    """
+
+    pass
+
+    def __init__(self,first_name,last_name,password):
+
+            '''
+            __init__ method that helps us define properties for our objects.
+
+            Args:
+                first_name: New account first name.
+                last_name : New account last name.
+                password : New account password address.
+            '''
