ea7ab971e2432cb8352f05982e777fb3a7d2b4a7
vique254 <victoronyango051@gmail.com>
2019-11-11 09:25:35 +0300

    import python test framework and create a subclass

---
+++ user_test.py
@@ -0,0 +1,10 @@
+import unittest # Importing the unittest module
+from password import Account # Importing the Account class
+
+class TestAccount(unittest.TestCase):
+    '''
+    Test class that defines test cases for the account class behaviours.
+    
+    Args:
+    unittest.TestCase: TestCase class that helps in creating  test cases
+    '''
